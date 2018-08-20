6a910aca47259541290e92102fd5ee3c31a27628
apfesta <andy_festa@yahoo.com>
2018-08-19 20:57:13 -0400

    Initial Project Setup

---
+++ src/main/java/net/mixednutz/api/model/NetworkInfo.java
@@ -0,0 +1,21 @@
+package net.mixednutz.api.model;
+
+public class NetworkInfo {
+
+	private String displayName;
+	private String hostName;
+	
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	public String getHostName() {
+		return hostName;
+	}
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+	
+}
