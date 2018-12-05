1d3713f42b69de454c1c2e9c50acf212e5dc44ef
apfesta <andy_festa@yahoo.com>
2018-12-05 13:28:42 -0500

    I realized I wanted a string id as part of the network info

--- src/main/java/net/mixednutz/api/model/INetworkInfoSmall.java
+++ src/main/java/net/mixednutz/api/model/INetworkInfoSmall.java
@@ -3,21 +3,32 @@ package net.mixednutz.api.model;
 public interface INetworkInfoSmall {
 
 	/**
-	 * Name of this social network
+	 * Name of this social network.  (Ex. MixedNutz.net)
 	 * 
 	 * @return
 	 */
 	public String getDisplayName();
 	
 	/**
-	 * Name of the host/server.  Maybe be used as a unique ID.
+	 * Name of the host/server. (Ex. mixednutz.net)
+	 * Include domain name without scheme or ports.
 	 * 
 	 * @return
 	 */
 	public String getHostName();
 	
 	/**
+	 * ID of the social network.  IDs should be text-based and contain no special
+	 * characters.  (Ex. mixednutz)
+	 * 
+	 * @return
+	 */
+	public String getId();
+	
+	/**
 	 * Get the FontAwesome Icon name (Optional)
+	 * Do not include the "fa-" prefix. (Ex. twitter)
+	 * 
 	 * @return
 	 */
 	public String getFontAwesomeIconName();
