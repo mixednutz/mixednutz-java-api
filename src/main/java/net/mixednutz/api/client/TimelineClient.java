40174c04775207adf01a529eeb70c5132f899cb0
apfesta <andy_festa@yahoo.com>
2019-01-13 21:27:06 -0500

    added string token version of the getTimeline methods

--- src/main/java/net/mixednutz/api/client/TimelineClient.java
+++ src/main/java/net/mixednutz/api/client/TimelineClient.java
@@ -31,6 +31,18 @@ public interface TimelineClient<Token> {
 	IPage<? extends ITimelineElement, Token> getTimeline(IPageRequest<Token> pagination);
 	
 	/**
+	 * Retrieve a timeline of all content subscribed to for this given network.
+	 * 
+	 * This provides a String token and allows the service to parse it.
+	 * 
+	 * @param username
+	 * @param pagination
+	 * @param pageSize
+	 * @return
+	 */
+	IPage<? extends ITimelineElement, Token> getTimelineStringToken(IPageRequest<String> pagination);
+	
+	/**
 	 * Retrieve a timeline of all public to for this given network.
 	 * 
 	 * @param username
@@ -47,6 +59,18 @@ public interface TimelineClient<Token> {
 	 * @return
 	 */
 	IPage<? extends ITimelineElement, Token> getPublicTimeline(IPageRequest<Token> pagination);
+	
+	/**
+	 * Retrieve a timeline of all public to for this given network.
+	 * 
+	 * This provides a String token and allows the service to parse it.
+	 * 
+	 * @param username
+	 * @param pagination
+	 * @param pageSize
+	 * @return
+	 */
+	IPage<? extends ITimelineElement, Token> getPublicTimelineStringToken(IPageRequest<String> pagination);
 	
 	
 
--- src/main/java/net/mixednutz/api/client/UserClient.java
+++ src/main/java/net/mixednutz/api/client/UserClient.java
@@ -40,6 +40,18 @@ public interface UserClient<Token> {
 	 */
 	IPage<? extends ITimelineElement, Token> getUserTimeline(IPageRequest<Token> pagination);
 	
+	/**
+	 * Retrieve a timeline for the current user's content to for this given network.
+	 * 
+	 * This provides a String token and allows the service to parse it.
+	 * 
+	 * @param username
+	 * @param pagination
+	 * @param pageSize
+	 * @return
+	 */
+	IPage<? extends ITimelineElement, Token> getUserTimelineStringToken(IPageRequest<String> pagination);
+	
 	
 	/**
 	 * Retrieve a timeline of user's content to for this given network.
@@ -58,6 +70,18 @@ public interface UserClient<Token> {
 	 * @return
 	 */
 	IPage<? extends ITimelineElement, Token> getUserTimeline(String username, IPageRequest<Token> pagination);
+	
+	/**
+	 * Retrieve a timeline of user's content to for this given network.
+	 * 
+	 * This provides a String token and allows the service to parse it.
+	 * 
+	 * @param username
+	 * @param pagination
+	 * @param pageSize
+	 * @return
+	 */
+	IPage<? extends ITimelineElement, Token> getUserTimelineStringToken(String username, IPageRequest<String> pagination);
 	
 	void subscribeToUser(String username);
 }
