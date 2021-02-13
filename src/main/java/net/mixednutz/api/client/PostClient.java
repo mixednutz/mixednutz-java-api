80ee14082fd997098df854688817462505f229a0
apfesta <andy_festa@yahoo.com>
2021-02-13 00:23:37 -0500

    added ability to create a tweet

--- src/main/java/net/mixednutz/api/client/PostClient.java
+++ src/main/java/net/mixednutz/api/client/PostClient.java
@@ -6,4 +6,11 @@ public interface PostClient<P extends IPost> {
 
 	void postToTimeline(P post);
 	
+	/**
+	 * Creates a new instance of a post to be filled
+	 * 
+	 * @return
+	 */
+	P create();
+	
 }
