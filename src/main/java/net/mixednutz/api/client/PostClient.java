51830e0bcbeaad866c15fb8d7765e65784c0a6c1
apfesta <andy_festa@yahoo.com>
2021-06-21 22:40:13 -0400

    added a return value to the postclient post method

--- src/main/java/net/mixednutz/api/client/PostClient.java
+++ src/main/java/net/mixednutz/api/client/PostClient.java
@@ -3,10 +3,17 @@ package net.mixednutz.api.client;
 import java.util.Map;
 
 import net.mixednutz.api.model.IPost;
+import net.mixednutz.api.model.ITimelineElement;
 
 public interface PostClient<P extends IPost> {
 
-	void postToTimeline(P post);
+	/**
+	 * Posts to a timeline
+	 * 
+	 * @param post
+	 * @return
+	 */
+	ITimelineElement postToTimeline(P post);
 	
 	/**
 	 * Creates a new instance of a post to be filled
